a41bdc5bc111ea47261b572b3003d5f15738182b
Omar Limbert Huanca Sanchez <omar@programadorez.com>
2018-08-16 15:16:23 -0400

    deploy configuration updated

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-JAR_NAME=$1
-USER_HOME=$2
-echo 'Stopping Tomcat service...'	
-echo $JAR_NAME	
-echo $USER_HOME
-sudo service tomcat stop
-echo 'Sending jar file... '
-sudo cp -f /tmp/$JAR_NAME.jar $USER_HOME/$JAR_NAME.jar
-sudo nohup java -jar $USER_HOME/$JAR_NAME.jar &
